2d4fc4bd9cc9ec05ce02554cd7f5c1ee8db4fe81
Jerin Joseph <jerin.joseph@oracle.com>
2021-12-04 20:17:03 +0530

    Set up Currency Exchange service
    Use the RestTemplate to communicate between services

---
+++ currency-exchange-service/src/main/java/com/jerin/spring/microservices/CurrencyExchangeController.java
@@ -0,0 +1,26 @@
+package com.jerin.spring.microservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+
+	@Autowired
+	private Environment environment;
+
+	@Autowired
+	private CurrencyExchangeRepository repository;
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange getCurrencyExchange(@PathVariable String from, @PathVariable String to) {
+
+		CurrencyExchange exchange = repository.findByFromAndTo(from, to);
+		String port = environment.getProperty("local.server.port");
+		exchange.setEnvironment(port);
+		return exchange;
+	}
+}
---
+++ currency-exchange-service/src/main/java/com/jerin/spring/microservices/CurrencyExchangeRepository.java
@@ -0,0 +1,9 @@
+package com.jerin.spring.microservices;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+	CurrencyExchange findByFromAndTo(String from, String to);
+
+}
