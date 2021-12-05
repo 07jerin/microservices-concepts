766e0310f3dd235d357f550c01338ab1a781ce31
Jerin Joseph <jerin.joseph@oracle.com>
2021-12-05 17:34:12 +0530

    Dsitributed tracing with zipkin / sleuth

--- currency-exchange-service/src/main/java/com/jerin/spring/microservices/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/jerin/spring/microservices/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.jerin.spring.microservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+
+	Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
 	@Autowired
 	private Environment environment;
@@ -18,6 +22,7 @@ public class CurrencyExchangeController {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange getCurrencyExchange(@PathVariable String from, @PathVariable String to) {
 
+		logger.info("getCurrencyExchange called with {} to {}", from, to);
 		CurrencyExchange exchange = repository.findByFromAndTo(from, to);
 		String port = environment.getProperty("local.server.port");
 		exchange.setEnvironment(port);
